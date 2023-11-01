bd10bc9212f3aa64cb673bba94289bce38cbc7f7
EvilGiraffes <thomas.e.brekke@hotmail.no>
2023-11-01 20:45:59 +0100

    Added an aggregate preprocessor. Muliple preprocessors can be used within this processor.

--- lingo_morph/src/preprocessing.rs
+++ lingo_morph/src/preprocessing.rs
@@ -1,5 +1,8 @@
 use std::fmt::Display;
 
+#[cfg(feature = "logging")]
+use log::{ trace, info };
+
 pub trait Preprocessor: Display {
     fn next(&self, character: char) -> Option<char>;
 }
@@ -19,3 +22,31 @@ impl Display for NullPreprocessor {
     }
 }
 
+pub struct AggregatePreprocessor {
+    processors: Vec<Box<dyn Preprocessor>>,
+}
+
+impl Preprocessor for AggregatePreprocessor {
+    fn next(&self, character: char) -> Option<char> {
+        #[cfg(feature = "logging")]
+        info!("Preprocessing with multiple processors");
+        let mut current: Option<char> = Some(character);
+        for processor in self.processors.iter() {
+            #[cfg(feature = "logging")]
+            trace!("Current processor is {}", processor);
+            current = current.and_then(|inner| processor.next(inner));
+        }
+        current
+    }
+}
+
+impl Display for AggregatePreprocessor {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "AggregatePreprocessor with: ")?;
+        let length = self.processors.len();
+        for i in 0..length - 1 {
+            write!(f, "{}, ", self.processors[i])?;
+        }
+        write!(f, "{}", self.processors[length - 1])
+    }
+}
