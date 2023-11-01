cb7f2e3d2544ce2e1d54e9dfca0a84a920143c4c
EvilGiraffes <thomas.e.brekke@hotmail.no>
2023-11-01 19:21:22 +0100

    Fixed trait implementation where it uses self and not a static function.

--- lingo_morph/src/preprocessing.rs
+++ lingo_morph/src/preprocessing.rs
@@ -1,10 +1,11 @@
 pub trait Preprocessor {
-    fn next(character: char) -> Option<char>;
+    fn next(&self, character: char) -> Option<char>;
 }
 
 pub struct NullPreprocessor;
 impl Preprocessor for NullPreprocessor {
-    fn next(character: char) -> Option<char> {
+    fn next(&self, character: char) -> Option<char> {
         Some(character)
     }
 }
+
