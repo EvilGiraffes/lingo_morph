a148acc795b919a1dffa2a43801e1ca817c728b4
EvilGiraffes <thomas.e.brekke@hotmail.no>
2023-11-01 20:45:09 +0100

    Added display to the null processor.

--- lingo_morph/src/preprocessing.rs
+++ lingo_morph/src/preprocessing.rs
@@ -1,11 +1,21 @@
-pub trait Preprocessor {
+use std::fmt::Display;
+
+pub trait Preprocessor: Display {
     fn next(&self, character: char) -> Option<char>;
 }
 
+#[derive(Debug)]
 pub struct NullPreprocessor;
+
 impl Preprocessor for NullPreprocessor {
     fn next(&self, character: char) -> Option<char> {
         Some(character)
+    }
+}
+
+impl Display for NullPreprocessor {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "NullProcessor")
     }
 }
 
