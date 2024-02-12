e490bb9885886a5df4a86e2f4fc81cd4ef264293
EvilGiraffes <thomas.e.brekke@hotmail.no>
2024-02-12 21:29:37 +0100

    Implemented map

--- lingo_morph/src/processors.rs
+++ lingo_morph/src/processors.rs
@@ -1,3 +1,38 @@
 pub trait Processor<I> {
     type Output;
     fn process(&mut self, given: I) -> Self::Output;
+    fn map<F, R>(self, mapper: F) -> Map<Self, F>
+    where
+        Self: Sized,
+        F: FnMut(Self::Output) -> R
+    {
+        map(self, mapper)
+    }
+
+pub struct Map<P, F> {
+    processor: P,
+    map: F,
+}
+
+impl<P, I, F, O, R> Processor<I> for Map<P, F>
+where
+    P: Processor<I, Output = O>,
+    F: FnMut(O) -> R,
+{
+    type Output = R;
+    fn process(&mut self, given: I) -> Self::Output {
+        (self.map)(self.processor.process(given))
+    }
+}
+
+pub fn map<P, F, I, O, R>(processor: P, map: F) -> Map<P, F>
+where
+    P: Processor<I, Output = O>,
+    F: FnMut(O) -> R,
+{
+    Map {
+        processor,
+        map,
+    }
+}
+
