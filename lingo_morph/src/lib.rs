9825291965016f8320e20bba911203b3ac3d229f
EvilGiraffes <thomas.e.brekke@hotmail.no>
2024-02-12 19:21:59 +0100

    Restart

--- lingo_morph/src/lib.rs
+++ lingo_morph/src/lib.rs
@@ -1,1 +1,3 @@
-pub mod preprocessing;
+// This mimics the log crate to avoid checking for the feature available
+#[macro_use]
+mod log;
---
+++ lingo_morph/src/log.rs
@@ -0,0 +1,54 @@
+// TODO remove
+#![allow(unused)]
+
+macro_rules! trace {
+    ($($args:tt)+) => {{
+        if cfg!(feature = "logging") {
+            _log::trace!($($args)+);
+        }
+    }};
+}
+
+macro_rules! debug {
+    ($($args:tt)+) => {{
+        if cfg!(feature = "logging") {
+            _log::debug!($($args)+);
+        }
+    }};
+}
+
+macro_rules! info {
+    ($($args:tt)+) => {{
+        if cfg!(feature = "logging") {
+            _log::info!($($args)+);
+        }
+    }};
+}
+
+
+macro_rules! warn {
+    ($($args:tt)+) => {{
+        if cfg!(feature = "logging") {
+            _log::warn!($($args)+);
+        }
+    }};
+}
+
+
+macro_rules! error {
+    ($($args:tt)+) => {{
+        if cfg!(feature = "logging") {
+            _log::error!($($args)+);
+        }
+    }};
+}
+
+
+macro_rules! log_enabled {
+    ($($args:tt)+) => {{
+        if cfg!(feature = "logging") {
+            _log::log_enabled!($($args)+);
+        }
+    }};
+}
+
