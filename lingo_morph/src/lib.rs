7a92d6f62e48efdba27c12b271cf504b77fb1c9a
EvilGiraffes <thomas.e.brekke@hotmail.no>
2024-02-13 19:39:28 +0100

    Changed signature for processor

--- lingo_morph/src/lib.rs
+++ lingo_morph/src/lib.rs
@@ -1,6 +1,4 @@
-pub use crate::monads::Either;
 pub mod processors;
-mod monads;
 // This mimics the log crate to avoid checking for the feature available
 #[macro_use]
 mod log;
--- lingo_morph/src/processors.rs
+++ lingo_morph/src/processors.rs
@@ -1,62 +1,59 @@
-use crate::Either;
-
+pub type Processed<O, R> = (O, R);
 pub type Compose<A, B> = Pipe<B, A>;
 pub type RightIgnore<L, R> = LeftIgnore<R, L>;
 
-pub trait Processor<I> {
+pub trait Processor<I>: Sized {
     type Output;
-    fn process(&mut self, given: I) -> Self::Output;
-    fn map<F, R>(self, mapper: F) -> Map<Self, F>
+    fn process(&mut self, given: I) -> Processed<Self::Output, I>;
+    fn map<F, R>(self, map: F) -> Map<Self, F>
     where
-        Self: Sized,
         F: FnMut(Self::Output) -> R
     {
-        map(self, mapper)
+        Map {
+            processor: self,
+            map,
+        }
     }
     // TODO: filter
     //       bind
     //       swap
-    fn pipe<P, INT, PO>(self, other: P) -> Pipe<Self, P>
+    fn pipe<P, PO>(self, other: P) -> Pipe<Self, P>
     where
-        Self: Sized + Processor<I, Output = INT>,
-        P: Processor<INT, Output = PO>,
+        P: Processor<I, Output = PO>,
     {
-        pipe(self, other)
+        Pipe(self, other)
     }
-    fn compose<P, PI>(self, other: P) -> Compose<Self, P>
+    fn compose<P, PO>(self, other: P) -> Compose<Self, P>
     where
-        Self: Sized,
-        P: Processor<PI, Output = I>,
+        P: Processor<I, Output = PO>,
     {
-        compose(self, other)
+        other.pipe(self)
     }
     fn left_ignore<P, PO>(self, other: P) -> LeftIgnore<Self, P>
     where
-        Self: Sized,
         P: Processor<I, Output = PO>,
     {
-        left_ignore(self, other)
+        LeftIgnore(self, other)
     }
     fn right_ignore<P, PO>(self, other: P) -> RightIgnore<Self, P>
     where
-        Self: Sized,
         P: Processor<I, Output = PO>,
     {
-        right_ignore(self, other)
+        other.left_ignore(self)
     }
     fn left_or<P, O>(self, other: P) -> Or<Self, P>
     where
-        Self: Sized + Processor<I, Output = Either<O, I>>,
+        Self: Processor<I, Output = Option<O>>,
         P: Processor<I, Output = O>,
     {
-        left_or(self, other)
+        Or(self, other)
     }
-    fn right_or<P>(self, other: P) -> Or<P, Self>
+    fn right_or<P, O>(self, other: P) -> Or<P, Self>
     where
-        Self: Sized,
-        P: Processor<I, Output = Either<Self::Output, I>>,
+        Self: Processor<I, Output = O>,
+        P: Processor<I, Output = Option<O>>,
     {
-        right_or(self, other)
+        other.left_or(self)
     }
 }
 
@@ -71,22 +68,25 @@ where
     F: FnMut(O) -> R,
 {
     type Output = R;
-    fn process(&mut self, given: I) -> Self::Output {
-        (self.map)(self.processor.process(given))
+    fn process(&mut self, given: I) -> Processed<Self::Output, I> {
+        let (processed, rest) = self.processor.process(given);
+        let mapped = (self.map)(processed);
+        (mapped, rest)
     }
 }
 
 pub struct Pipe<A, B>(A, B);
 
-impl<A, B, AI, INT, BO> Processor<AI> for Pipe<A, B>
+impl<A, B, I, AO, BO> Processor<I> for Pipe<A, B>
 where
-    A: Processor<AI, Output = INT>,
-    B: Processor<INT, Output = BO>,
+    A: Processor<I, Output = AO>,
+    B: Processor<I, Output = BO>,
 {
-    type Output = BO;
-    fn process(&mut self, given: AI) -> Self::Output {
-        let intermediate = self.0.process(given);
-        self.1.process(intermediate)
+    type Output = (AO, BO);
+    fn process(&mut self, given: I) -> Processed<Self::Output, I> {
+        let (first, rest) = self.0.process(given);
+        let (second, rest) = self.1.process(rest);
+        ((first, second), rest)
     }
 }
 
@@ -94,14 +94,13 @@ pub struct LeftIgnore<L, R>(L, R);
 
 impl<L, R, I, LO, RO> Processor<I> for LeftIgnore<L, R>
 where
-    I: Copy,
     L: Processor<I, Output = LO>,
     R: Processor<I, Output = RO>,
 {
     type Output = RO;
-    fn process(&mut self, given: I) -> Self::Output {
-        _ = self.0.process(given);
-        self.1.process(given)
+    fn process(&mut self, given: I) -> Processed<Self::Output, I> {
+        let (_, rest) = self.0.process(given);
+        self.1.process(rest)
     }
 }
 
@@ -109,72 +108,15 @@ pub struct Or<A, B>(A, B);
 
 impl<A, B, I, O> Processor<I> for Or<A, B>
 where
-    A: Processor<I, Output = Either<O, I>>,
+    A: Processor<I, Output = Option<O>>,
     B: Processor<I, Output = O>,
 {
     type Output = O;
-    fn process(&mut self, given: I) -> Self::Output {
-        self.0
-            .process(given)
-            .lunwrap_or_map(|given| self.1.process(given))
+    fn process(&mut self, given: I) -> Processed<Self::Output, I> {
+        match self.0.process(given) {
+            (Some(value), rest) => (value, rest),
+            (None, rest) => self.1.process(rest),
+        }
     }
 }
 
-pub fn map<P, F, I, O, R>(processor: P, map: F) -> Map<P, F>
-where
-    P: Processor<I, Output = O>,
-    F: FnMut(O) -> R,
-{
-    Map {
-        processor,
-        map,
-    }
-}
-
-pub fn pipe<A, B, AI, INT, BO>(from: A, into: B) -> Pipe<A, B>
-where
-    A: Processor<AI, Output = INT>,
-    B: Processor<INT, Output = BO>,
-{
-    Pipe(from, into)
-}
-
-pub fn compose<A, B, AO, INT, BI>(from: A, into: B) -> Compose<A, B>
-where
-    A: Processor<INT, Output = AO>,
-    B: Processor<BI, Output = INT>,
-{
-    pipe(into, from)
-}
-
-pub fn left_ignore<L, R, I, LO, RO>(left: L, right: R) -> LeftIgnore<L, R>
-where
-    L: Processor<I, Output = LO>,
-    R: Processor<I, Output = RO>,
-{
-    LeftIgnore(left, right)
-}
-
-pub fn right_ignore<L, R, I, LO, RO>(left: L, right: R) -> RightIgnore<L, R>
-where
-    L: Processor<I, Output = LO>,
-    R: Processor<I, Output = RO>,
-{
-    left_ignore(right, left)
-}
-
-pub fn left_or<A, B, I, O>(left: A, right: B) -> Or<A, B>
-where
-    A: Processor<I, Output = Either<O, I>>,
-    B: Processor<I, Output = O>,
-{
-    Or(left, right)
-}
-
-pub fn right_or<A, B, I, O>(left: A, right: B) -> Or<B, A>
-where
-    A: Processor<I, Output = O>,
-    B: Processor<I, Output = Either<O, I>>,
-{
-    left_or(right, left)
-}
