ed1c541d1298a2f14d2641be9f871d44efeac67f
EvilGiraffes <thomas.e.brekke@hotmail.no>
2024-03-05 21:22:33 +0100

    Implemented char processor

--- lingo_morph/src/processors.rs
+++ lingo_morph/src/processors.rs
@@ -1,1 +1,20 @@
-// Reserved for concrete processors
+use std::error::Error;
+
+use crate::{source::Source, Processed, Processor, Status};
+
+pub struct Char(char);
+
+impl Processor<char> for Char {
+    type Output = char;
+    fn process<S>(&mut self, mut given: S) -> Processed<Self::Output, S>
+    where
+        S: Source<Item = char>,
+        S::RollBackErr: Error,
+    {
+        match given.next_if_eq(&self.0) {
+            Some(next) => Ok(Status::Done(next, given)),
+            None => Ok(Status::Mismatch(given)),
+        }
+    }
+}
+
