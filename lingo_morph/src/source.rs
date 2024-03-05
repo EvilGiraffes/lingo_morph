9def7b6b4cc8c1377f2211101f5d07b825e6f81e
EvilGiraffes <thomas.e.brekke@hotmail.no>
2024-03-05 19:39:53 +0100

    Added Source.peek_mut

--- lingo_morph/src/source.rs
+++ lingo_morph/src/source.rs
@@ -2,9 +2,10 @@ pub trait Source: Iterator {
     type RollBackErr;
     fn roll_back(&mut self, by: usize) -> Result<(), Self::RollBackErr>;
     fn peek(&mut self) -> Option<&Self::Item>;
+    fn peek_mut(&mut self) -> Option<&mut Self::Item>;
     fn next_if<P>(&mut self, predicate: P) -> Option<Self::Item>
     where
-        P: FnOnce(&Self::Item) -> bool
+        P: FnOnce(&Self::Item) -> bool,
     {
         let peeked = self.peek()?;
         if predicate(peeked) {
