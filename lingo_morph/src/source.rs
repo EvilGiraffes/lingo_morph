348a9ba15a80d0bed3d8c2ef59a6fa253fd81015
EvilGiraffes <thomas.e.brekke@hotmail.no>
2024-03-05 19:39:20 +0100

    Fixed: Source.peek returns a ref

--- lingo_morph/src/source.rs
+++ lingo_morph/src/source.rs
@@ -1,13 +1,13 @@
 pub trait Source: Iterator {
     type RollBackErr;
     fn roll_back(&mut self, by: usize) -> Result<(), Self::RollBackErr>;
-    fn peek(&mut self) -> Option<Self::Item>;
+    fn peek(&mut self) -> Option<&Self::Item>;
     fn next_if<P>(&mut self, predicate: P) -> Option<Self::Item>
     where
         P: FnOnce(&Self::Item) -> bool
     {
         let peeked = self.peek()?;
-        if predicate(&peeked) {
+        if predicate(peeked) {
             self.next()
         } else {
             None
