49965044c45d04cf33cbfe289b19d0b6af5e053c
EvilGiraffes <thomas.e.brekke@hotmail.no>
2024-03-05 21:22:58 +0100

    Implemented unsigned single digit processor

--- lingo_morph/src/processors.rs
+++ lingo_morph/src/processors.rs
@@ -18,3 +18,27 @@ impl Processor<char> for Char {
     }
 }
 
+macro_rules! single_char_processor {
+    ($type:ty, $name: ident) => {
+        pub fn $name(number: u8) -> Option<impl Processor<char, Output = $type>> {
+            match number {
+                0 => Some(Char('0').replace(0)),
+                1 => Some(Char('1').replace(1)),
+                2 => Some(Char('2').replace(2)),
+                3 => Some(Char('3').replace(3)),
+                4 => Some(Char('4').replace(4)),
+                5 => Some(Char('5').replace(5)),
+                6 => Some(Char('6').replace(6)),
+                7 => Some(Char('7').replace(7)),
+                8 => Some(Char('8').replace(8)),
+                9 => Some(Char('9').replace(9)),
+                _ => None,
+            }
+        }
+    };
+}
+
+single_char_processor!(u8, single_u8);
+single_char_processor!(u32, single_u32);
+single_char_processor!(u64, single_u64);
+
