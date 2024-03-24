74862f34d150eab34a2948694ebb8eb724a072e1
EvilGiraffes <thomas.e.brekke@hotmail.no>
2024-03-24 20:36:47 +0100

    Implemented CharRange

--- lingo_morph/src/processors.rs
+++ lingo_morph/src/processors.rs
@@ -1,3 +1,5 @@
+use std::ops::{Bound, RangeBounds};
+
 use crate::{source::Source, Processed, Processor, Status};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
@@ -17,8 +19,48 @@ impl Processor<char> for Char {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct CharRange {
+    start: Bound<char>,
+    end: Bound<char>,
+}
+
+impl RangeBounds<char> for CharRange {
+    fn start_bound(&self) -> Bound<&char> {
+        self.start.as_ref()
+    }
+
+    fn end_bound(&self) -> Bound<&char> {
+        self.end.as_ref()
+    }
+}
+
+impl Processor<char> for CharRange {
+    type Output = char;
+
+    fn process<S>(&mut self, mut given: S) -> Processed<Self::Output, S>
+    where
+        S: Source<Item = char>,
+    {
+        match given.next_if(|item| self.contains(item)) {
+            Some(next) => Ok(Status::Done(next, given)),
+            None => Ok(Status::Mismatch(given)),
+        }
+    }
+}
+
 pub fn char(from: char) -> Char {
     Char(from)
+}
+
+pub fn char_range<R>(range: R) -> CharRange
+where
+    R: RangeBounds<char>,
+{
+    CharRange {
+        start: range.start_bound().cloned(),
+        end: range.end_bound().cloned(),
+    }
 }
 
 pub fn digit(digit: u8) -> Option<impl Processor<char, Output = u8>> {
