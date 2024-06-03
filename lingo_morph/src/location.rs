7c3ca5ac57e95721e97ba516ae5c4022a35f197b
EvilGiraffes <thomas.e.brekke@hotmail.no>
2024-06-03 19:26:19 +0200

    Failed revert lol

--- lingo_morph/src/location.rs
+++ lingo_morph/src/location.rs
@@ -1,72 +1,3 @@
-<<<<<<< HEAD
-use std::{error::Error, fmt::Display, num::NonZeroUsize};
-
-use crate::buf::RingBuf;
-
-pub trait Tracker {
-    type DecrementErr: Error + 'static;
-
-    fn location(&self) -> Location;
-
-    fn inc_from_slice(&mut self, from: &[u8]);
-
-    fn dec_to(&mut self, to: Location) -> Result<(), Self::DecrementErr>;
-
-    fn inc_from_u32(&mut self, from: u32) {
-        self.inc_from_slice(&from.to_le_bytes())
-    }
-
-    fn inc_from_char(&mut self, from: char) {
-        self.inc_from_u32(from as u32)
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct OutOfBufferedRange;
-
-impl Display for OutOfBufferedRange {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "out of buffered range")
-    }
-}
-
-impl Error for OutOfBufferedRange {}
-
-#[derive(Debug, Clone)]
-pub struct Utf8 {
-    passed: RingBuf<Location>,
-    current: BufferLocation,
-}
-
-impl Tracker for Utf8 {
-    type DecrementErr = OutOfBufferedRange;
-
-    fn location(&self) -> Location {
-        self.current
-            .get(&self.passed)
-            .map(|x| *x)
-            .unwrap_or_else(|| Location::default())
-    }
-
-    // TODO: Implement these
-
-    fn inc_from_slice(&mut self, from: &[u8]) {
-        unimplemented!("Utf8 inc_from_slice is not implemented yet")
-    }
-
-    fn inc_from_char(&mut self, from: char) {
-        // This will be implemented as it is a simpler implementation due to the way utf8 works in
-        // rust already
-        unimplemented!("Utf8 inc_from_char is not implemented yet")
-    }
-
-    fn dec_to(&mut self, to: Location) -> Result<(), Self::DecrementErr> {
-        unimplemented!("Utf8 dec_to is not implemented yet")
-    }
-}
-
-=======
->>>>>>> parent of 4050d81 (Added tracker)
 #[derive(Debug, Default, Clone, Copy)]
 pub struct Location {
     line: usize,
@@ -110,22 +41,5 @@ impl Location {
     pub unsafe fn set_at_bytes(&mut self, at_bytes: usize) -> &mut Self {
         self.at_bytes = at_bytes;
         self
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-enum BufferLocation {
-    Head,
-    At(NonZeroUsize),
-    Last,
-}
-
-impl BufferLocation {
-    fn get<'a, T>(&'a self, from: &'a RingBuf<T>) -> Option<&T> {
-        match self {
-            BufferLocation::Head => from.head(),
-            BufferLocation::At(idx) => from.get(idx.get()),
-            BufferLocation::Last => from.tail(),
-        }
     }
 }
